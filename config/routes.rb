bdabc5584e13095a91702bb5353591b0171868e6
oumsamon <oumsamon@gmail.com>
2021-10-26 20:09:18 -0500

    set up CRUD for /todos

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/hello", to: "first#hello"
+
+  resource :todos
+  #resource will auto full CRUD routes - could be check in terminal rails routes
 end
